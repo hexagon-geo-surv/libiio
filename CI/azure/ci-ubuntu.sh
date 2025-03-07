b87f4d88d563182c93d51da08ba534c5226ac176
Dan Nechita <dan.nechita@analog.com>
2025-03-07 19:01:10 +0200

    CI: do a test where make uses exactly one core
    Signed-off-by: Dan Nechita <dan.nechita@analog.com>

--- CI/azure/ci-ubuntu.sh
+++ CI/azure/ci-ubuntu.sh
@@ -4,6 +4,6 @@ uname -a
 echo "$PWD"
 mkdir build && cd build
 cmake .. -Werror=dev -DCOMPILE_WARNING_AS_ERROR=ON -DWITH_SERIAL_BACKEND=ON -DWITH_EXAMPLES=ON -DPYTHON_BINDINGS=ON -DCPP_BINDINGS=ON -DENABLE_PACKAGING=ON -DCPACK_SYSTEM_NAME="${ARTIFACTNAME}"
-make
+make -j1
 make package
 make required2tar
