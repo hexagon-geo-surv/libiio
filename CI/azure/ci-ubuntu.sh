9a46784c546eb43b7af3e555d18e2e16f5691fe1
Dan Nechita <dan.nechita@analog.com>
2025-04-14 15:25:37 +0300

    CI: Install gcc version 10 and set it as the default gcc
    Signed-off-by: Dan Nechita <dan.nechita@analog.com>

--- CI/azure/ci-ubuntu.sh
+++ CI/azure/ci-ubuntu.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 set -x
 uname -a
+sudo apt update
+sudo apt install -y gcc-10
+sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-10 100
 echo "$PWD"
 mkdir build && cd build
 cmake .. -Werror=dev -DCOMPILE_WARNING_AS_ERROR=ON -DWITH_SERIAL_BACKEND=ON -DWITH_EXAMPLES=ON -DPYTHON_BINDINGS=ON -DCPP_BINDINGS=ON -DENABLE_PACKAGING=ON -DCPACK_SYSTEM_NAME="${ARTIFACTNAME}"
