fdab94f613371ead8489ba4682bdfbf72a283515
Dan Nechita <dan.nechita@analog.com>
2025-04-15 10:26:33 +0300

    CI: For arch: ppc64le add path of installed libiio dependencies
    Libiio dependencies such as: zstd, serialport, libaio, etc. cannot be found while running cmake. These are installed at /usr/lib/powerpc64le-linux-gnu. For some reason this path is not added to the default paths so we're adding it.
    
    Signed-off-by: Dan Nechita <dan.nechita@analog.com>

--- CI/azure/ci-ubuntu.sh
+++ CI/azure/ci-ubuntu.sh
@@ -4,6 +4,7 @@ uname -a
 sudo apt update
 sudo apt install -y gcc-10
 sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-10 100
+[ -n "$USR_LIB_ARCHNAME" ] && export LD_LIBRARY_PATH=/usr/lib/$USR_LIB_ARCHNAME
 echo "$PWD"
 mkdir build && cd build
 cmake .. -Werror=dev -DCOMPILE_WARNING_AS_ERROR=ON -DWITH_SERIAL_BACKEND=ON -DWITH_EXAMPLES=ON -DPYTHON_BINDINGS=ON -DCPP_BINDINGS=ON -DENABLE_PACKAGING=ON -DCPACK_SYSTEM_NAME="${ARTIFACTNAME}"
